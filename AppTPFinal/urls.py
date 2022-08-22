088f8eda006cc415831671a84b44231a9f56fbcd
Fernando Otero <f.o.otero@gmail.com>
2022-08-21 22:58:12 -0300

    Carga de musica y cine

--- AppTPFinal/urls.py
+++ AppTPFinal/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from AppTPFinal.views import Main, datos_crear_literatura, cargar_Musica, cargar_Cine, datos_Usuario, datos_crear_usuario
+from AppTPFinal.views import Main, cargar_literatura, cargar_Musica, cargar_Cine, datos_usuario, datos_crear_usuario
 
 urlpatterns = [
     path('',Main, name='TPFinalMain'),
     path('CrearUsuario/', datos_crear_usuario, name='TPFinalCrearUsuarios'),
-    path('DatosUsuario/', datos_Usuario, name='TPFinalMostrarUsuarios'),
-    path('CrearLiteratura/', datos_crear_literatura, name='TPFinalCrearliteratura'),
+    path('DatosUsuario/', datos_usuario, name='TPFinalMostrarUsuarios'),
+    path('CrearLiteratura/', cargar_literatura, name='TPFinalCargarliteratura'),
     path('Musica/', cargar_Musica, name='TPFinalCargarMusica'),
     path('Cine/>', cargar_Cine, name='TPFinalCargarCine'),
 
--- AppTPFinal/views.py
+++ AppTPFinal/views.py
@@ -3,36 +3,8 @@ import ctypes
 
 from django.shortcuts import render
 
-from AppTPFinal.forms import Usuario_Form, Literatura_Form
+from AppTPFinal.forms import Usuario_Form, Literatura_Form, Musica_Form, Cine_Form
 from AppTPFinal.models import Cine, Usuario, Musica, Literatura
-
-
-def cargar_Musica(request, nombre_artista_musica, nombre_disco_musica, anio_lanzamiento_musica, email_usuario_musica):
-    mus = Musica(nombre_artista_musica=nombre_artista_musica, nombre_disco_musica=nombre_disco_musica,
-                 anio_lanzamiento_musica=anio_lanzamiento_musica, email_usuario_musica=email_usuario_musica)
-    mus.save()
-    contexto = {
-        "nombre_artista_musica": mus.nombre_artista_musica,
-        "nombre_disco_musica": mus.nombre_disco_musica,
-        "anio_lanzamiento_musica": mus.anio_lanzamiento_musica,
-        "email_usuario_musica": mus.email_usuario_musica,
-        "fecha_creacion_musica": datetime.now()
-    }
-    return render(request, 'templateAutores.html', contexto)
-
-
-def cargar_Cine(request, nombre_pelicula_cine, nombre_director_cine, anio_lanzamiento_cine, email_usuario_cine):
-    edi = Cine(nombre_pelicula_cine=nombre_pelicula_cine, nombre_director_cine=nombre_director_cine,
-               anio_lanzamiento_cine=anio_lanzamiento_cine, email_usuario_cine=email_usuario_cine)
-    edi.save()
-    contexto = {
-        "nombre_pelicula_cine": nombre_pelicula_cine,
-        "nombre_director_cine": nombre_director_cine,
-        "anio_lanzamiento_cine": anio_lanzamiento_cine,
-        "email_usuario_cine": email_usuario_cine,
-        "fecha_crecion_cine": datetime.now()
-    }
-    return render(request, 'templateEditoriales.html', contexto)
 
 
 def Main(request):
@@ -46,7 +18,7 @@ def datos_crear_usuario(request):
     return render(request, 'AppCoder/usercrear.html', contexto)
 
 
-def datos_Usuario(request):
+def datos_usuario(request):
     usr = Usuario_Form(request.POST)
     if request.method == 'POST':
         if usr.is_valid():
@@ -63,7 +35,7 @@ def datos_Usuario(request):
     return render(request, 'AppCoder/datosusercreado.html', contexto)
 
 
-def datos_crear_literatura(request):
+def cargar_literatura(request):
     lit = Literatura_Form(request.POST)
     if request.method == 'POST':
         if lit.is_valid():
@@ -81,3 +53,41 @@ def datos_crear_literatura(request):
         'formularioliteratura': Literatura_Form()
     }
     return render(request, 'AppCoder/literatura.html', contexto)
+
+def cargar_Musica(request):
+    mus = Musica_Form(request.POST)
+    if request.method == 'POST':
+        if mus.is_valid():
+            mus = Musica(
+                nombre_artista_musica=request.POST.get('nombre_artista_musica'),
+                nombre_disco_musica=request.POST.get('nombre_disco_musica'),
+                anio_lanzamiento_musica=request.POST.get('anio_lanzamiento_musica'),
+                email_usuario_musica=request.POST.get('email_usuario_musica'),
+            )
+        mus.save()
+        ctypes.windll.user32.MessageBoxW(0, "Los datos se han cargado con exito", "mensaje", 0)
+
+    contexto = {
+        'formulariomusica': Musica_Form()
+    }
+    return render(request, 'AppCoder/musica.html', contexto)
+
+
+def cargar_Cine(request):
+    cine = Cine_Form(request.POST)
+    if request.method == 'POST':
+        if cine.is_valid():
+            cine = Cine(
+                nombre_pelicula_cine=request.POST.get('nombre_pelicula_cine'),
+                nombre_director_cine=request.POST.get('nombre_director_cine'),
+                anio_lanzamiento_cine=request.POST.get('anio_lanzamiento_cine'),
+                email_usuario_cine=request.POST.get('email_usuario_cine'),
+            )
+        cine.save()
+        ctypes.windll.user32.MessageBoxW(0, "Los datos se han cargado con exito", "mensaje", 0)
+
+    contexto = {
+        'formulariocine': Cine_Form()
+    }
+    return render(request, 'AppCoder/cine.html', contexto)
+
