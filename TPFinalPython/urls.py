c8b99b70486ffff9f3d1afbac56fcd21e02a112f
Fernando Otero <f.o.otero@gmail.com>
2022-09-10 17:10:02 -0300

    actualizado el requirement

--- TPFinalPython/urls.py
+++ TPFinalPython/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from AppTPFinal.views import Main
 
 urlpatterns = [
-    #path('', Main,),
+    path('', Main,),
     path('admin/', admin.site.urls),
     path('TPFinal/', include('AppTPFinal.urls')),
     path('TPFinal/', include('UserCoder.urls')),
