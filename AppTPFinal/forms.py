4bcc14426b8f6a8bc8ebc4e7813b97189c522c74
Fernando Otero <f.o.otero@gmail.com>
2022-09-09 21:22:25 -0300

    Subida

--- AppTPFinal/forms.py
+++ AppTPFinal/forms.py
@@ -1,3 +1,4 @@
+
 from django import forms
 from ckeditor.widgets import CKEditorWidget
 
--- TPFinalPython/settings.py
+++ TPFinalPython/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'AppTPFinal',
     'UserCoder',
     'ckeditor',
+
 ]
 
 MIDDLEWARE = [
